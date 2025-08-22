2813b6a9193a1ae019cd788402f3fa232a744d78
Satvik Chandra <satvikchandra100@gmail.com>
2025-08-22 11:51:47 +0530

    fix for update section and uploaded resume

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
 export const profilePicture =
   "https://res.cloudinary.com/dpnmetc7y/image/upload/v1712460910/portfoliov1-images/profile_pic.jpg";
 
-export const resumeLink =
+export const resumeLink = "/resume/Satvik_Chandra.pdf"
+
+export const resumeLinkDrive =
   "https://drive.google.com/file/d/1ulQb3z3nZA4wFbL4D05mBrVxqKiCyvvM/view?usp=sharing";
 
 export const GithubUrl = "https://github.com/SattuSupari21";
@@ -145,6 +147,40 @@ export const projects = [
       "https://res.cloudinary.com/dpnmetc7y/video/upload/v1712459770/portfoliov1-project-demos/styled-demo.mp4",
   },
 ];
+
+type ExperiencePositionIconType = "design" | "code" | "business" | "education";
+
+type ExperiencePositionItemType = {
+  /** Unique identifier for the position */
+  id: string;
+  /** The job title or position name */
+  title: string;
+  /** The period during which the position was held (e.g., "Jan 2020 - Dec 2021") */
+  employmentPeriod: string;
+  /** The type of employment (e.g., "Full-time", "Part-time", "Contract") */
+  employmentType?: string;
+  /** A brief description of the position or responsibilities */
+  description?: string;
+  /** An icon representing the position */
+  icon?: ExperiencePositionIconType;
+  /** A list of skills associated with the position */
+  skills?: string[];
+  /** Indicates if the position details are expanded in the UI */
+  isExpanded?: boolean;
+};
+
+type ExperienceItemType = {
+  /** Unique identifier for the experience item */
+  id: string;
+  /** Name of the company where the experience was gained */
+  companyName: string;
+  /** URL or path to the company's logo image */
+  companyLogo?: string;
+  /** List of positions held at the company */
+  positions: ExperiencePositionItemType[];
+  /** Indicates if this is the user's current employer */
+  isCurrentEmployer?: boolean;
+};
 
 export const WORK_EXPERIENCE: ExperienceItemType[] = [
   {
